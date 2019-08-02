c005f8796775c6ce00e813ddaaded3297c93b4b8
rambriksh <rambriksh009@gmail.com>
2019-08-02 21:47:46 +0530

    booking room

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HotelComponent } from './hotel/hotel.component';
+import { BookingRoomComponent } from './booking-room/booking-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    HotelComponent
+    HotelComponent,
+    BookingRoomComponent
   ],
   imports: [
     BrowserModule,
